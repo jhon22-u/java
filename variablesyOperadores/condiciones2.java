4f00a06d0a12b7eb3fe90804ac9f6ac9fff232ee
curso <tou@example.com>
2021-02-23 19:38:35 +0100

    trabajo 1

--- variablesyOperadores/condiciones2.java
+++ variablesyOperadores/condiciones2.java
@@ -10,9 +10,10 @@ public class condiciones2 {
 		Scanner numero = new Scanner(System.in);
 		int n1;
 		int v2;
+		int v3;
 		n1=numero.nextInt();
         v2=numero.nextInt();
-        
+        v3=numero.nextInt();
         
         if (n1> v2) 
         	 System.out.println(n1);
@@ -20,11 +21,18 @@ public class condiciones2 {
         
         else 
         	 System.out.println(v2);
+        numero.close();
         
-      
-
-	
+        if (n1> v3)
+             System.out.println(n1);
+        
+        else 
+                System.out.println(v3);
+        
+         
 	}
+	
+	
 	
 	
 	
