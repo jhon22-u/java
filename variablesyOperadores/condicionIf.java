21661ea359c7f6e3febd8d09121f365e52dd65ae
curso <tou@example.com>
2021-02-23 19:34:47 +0100

    trabajos en clase

---
+++ variablesyOperadores/condicionIf.java
@@ -0,0 +1,35 @@
+package variablesyOperadores;
+
+import java.util.Scanner;
+
+public class condicionIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* solo es un comando
+		 * if (comado)
+		 *  if (condicion){
+		 *  
+		 *    s1
+		 *    s2
+		 *    s2
+		 *    
+		 * }
+		 * else
+		 * 
+		 *   
+		 * 
+		 */
+		
+		System.out.println("porfavor nombre:");
+		
+		Scanner nombre= new Scanner(System.in);
+        String cadena = nombre.nextLine();
+        
+        System.out.println(cadena);
+        
+        
+
+	}   
+}
---
+++ variablesyOperadores/condiciones2.java
@@ -0,0 +1,32 @@
+package variablesyOperadores;
+
+import java.util.Scanner;
+
+public class condiciones2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner numero = new Scanner(System.in);
+		int n1;
+		int v2;
+		n1=numero.nextInt();
+        v2=numero.nextInt();
+        
+        
+        if (n1> v2) 
+        	 System.out.println(n1);
+        	 
+        
+        else 
+        	 System.out.println(v2);
+        
+      
+
+	
+	}
+	
+	
+	
+
+}
