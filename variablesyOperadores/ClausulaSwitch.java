a0b673d7c64d7cdb0c1c8c3de22f1ffb410d1e28
curso <tou@example.com>
2021-02-24 15:06:51 +0100

    trabajo final

---
+++ variablesyOperadores/ClausulaSwitch.java
@@ -0,0 +1,25 @@
+package variablesyOperadores;
+
+public class ClausulaSwitch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		int x=1;
+		switch(x) {
+		
+		}
+       		
+		
+
+	}
+	
+	
+	
+	
+	
+
+}
