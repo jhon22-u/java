c004549eba26133b2e0482ddc2ea7522d809456a
Jhon Triana <Jhon Triana@DESKTOP-TA09UQE>
2021-03-03 16:18:07 +0100

    hola

--- java.1/src/jejmplo1.java
+++ java.1/src/jejmplo1.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class jejmplo1 {
 
-	public static void main(String[] args) {
+	private static Object p;
+
+	public static void main(String[] args, Object numero) {
 		// TODO Auto-generated method stub
 
+		System.out.print("dame un numero: ");
+		System.out.print("dame un segundo numero: ");
+	     
+		System.out.println("Dime un numero: ");
+		Scanner entrada= new Scanner(System.in);
+		int num1= entrada.nextInt();
+		int num2= entrada.nextInt();
+		if (num1%num2==0 || num2%num1==0)
+		{
+			System.out.println("Son multiplos");
+		} else
+		{
+			System.out.println("No son multiplos");
+
+		}
+		entrada.close();
+
+		
 	}
 
 }
